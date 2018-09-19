5a7bc30ffa2eb5bb6b0247e31e825fca137c7272
Abhishek Arya <aa1488@singleton.cs.rutgers.edu>
2018-09-19 18:08:16 -0400

    aklsdjf

---
+++ main.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[]){
+	//mapred –-app [wordcount, sort] –-impl [procs, threads]
+	//--maps num_maps –-reduces num_reduces --input infile
+	//–-output outfile
+
+	printf("%i\n",strcmp("'hi","1945"));
+	if("hi"<"1945"){
+		printf("%s\n", "laksjdf");
+	}
+}
