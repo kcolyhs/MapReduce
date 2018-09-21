54a1421bf0907444167a6412b93195f4b69078bc
kcolyhs <bchen919@gmail.com>
2018-09-21 01:46:52 -0400

    Started fleshing out the dynamic arrays

--- wordcount.h
+++ wordcount.h
@@ -1,18 +1,37 @@
 struct WordVector{
 	char* word;
-	unisgned int count;
+	unsigned int count;
 }wordvec;
 struct VectorList{
 	int length;
+	int capacity;
 	wordvec* array;
 }veclist;
 
 struct TokenList{
 	int length;
+	int capacity;
 	char** array;
 }toklist;
+
+toklist* createTokList(int initlen){
+	toklist* newlist = (toklist*)malloc(sizeof(toklist));
+	newlist->length = 0;
+	newlist->capacity = initlen;
+	newlist.array = (char**)malloc(initlen*sizeof(char*));
+	return newlist;
+}
+void expandTokArray(toklist*){
+	char** newarr = (char**)malloc(newarr->capacity*2*sizeof(char*));
+	memcpy(newarr,toklist->array,toklist->capacity*sizeof(char*));
+	toklist->array.free();
+	toklist->array = newarr;
+}
+void 
+
+
 //Returns parsed input file
-toklist wcParseInput(char* inputfile){
+toklist* wcParseInput(char* inputfile){
 	return;
 }
 /*
