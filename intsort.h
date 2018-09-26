ce162e0c5c48855308ebadae77eee72b2a5a5434
sujaygami <sujaygami@Gmail.com>
2018-09-25 23:18:57 -0400

    Inserting Placeholder Data Structure/Functions for IntSort

---
+++ intsort.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+typedef struct IntList{
+	int length;
+	int capacity;
+	char** array;
+}intlist;
+
+//Placeholder for Integer List from Sort CSV File
+intlist* createIntList(int initlen){
+	intlist* newlist = (intlist*)malloc(sizeof(intlist));
+	newlist->length = 0;
+	newlist->capacity = initlen;
+	newlist->array = (char**)malloc(initlen*sizeof(char*));
+	return newlist;
+}
+
+//Placeholder to Dynamically Expand Integer List Array
+void expandIntArray(intlist* tlist){
+	char** newarr = (char**)malloc(tlist->capacity*2*sizeof(char*));
+	memcpy(newarr,tlist->array,tlist->capacity*sizeof(char*));
+	tlist->array = newarr;
+	tlist->capacity *= 2;
+}
+
+//Placeholder to add number to Integer List
+void addToIntlist(intlist* tlist,char* token){
+	if(tlist->length+1>tlist->capacity){
+		expandIntArray(tlist);
+	}
+	char* newtoken = (char*)malloc(strlen(token)+1);
+	strcpy(newtoken,token);
+	tlist->array[tlist->length]=newtoken;
+	tlist->length++;
+	return;
+}
