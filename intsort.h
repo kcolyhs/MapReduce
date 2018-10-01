c08e1f2ec856061a42392edcae541a7464cfdd8d
sujaygami <sujaygami@Gmail.com>
2018-10-01 00:32:27 -0400

    Int Sort Struct Framework In Place -- Todo Mapping & Merging

--- intsort.h
+++ intsort.h
@@ -36,6 +36,49 @@ void addToIntlist(intlist* tlist,char* token){
 	return;
 }
 
+//Basic struct to store value of integer in list
+typedef struct IntVector{
+	int value;
+}intvec;
+
+//Dynamic array for storing Integer Vector structs
+typedef struct IntList{
+	int length;
+	int capacity;
+	intvec* array;
+	pthread_mutex_t lock;
+}intvec_list;
+
+//Create New Dynamic Integer Vector List 
+intvec_list* createIntVecList(int initlen){
+	intvec_list* newlist = (intvec_list*)malloc(sizeof(intvec_list));
+	newlist->length = 0;
+	newlist->capacity = initlen;
+	newlist->array = (char**)malloc(initlen*sizeof(intvec));
+	return newlist;
+}
+
+//Expand Dynamic Array by Doubling Capacity
+void expandIntVecArray(intvec_list* vlist){
+	intvec* newarr = (intvec*)malloc(vlist->capacity*2*sizeof(intvec));
+	memcpy(newarr,vlist->array,vlist->capacity*sizeof(intvec));
+	free(vlist->array);
+	vlist->array = newarr;
+	vlist->capacity *= 2;
+}
+
+//Add New Integer from Parser into Dynamic Vector List
+void addIntToIntVecList(intvec_list* vlist, int value){
+	pthread_mutex_lock(&vlist->lock);
+	if(vlist->length+1>vlist->capacity){
+		expandVecArray(vlist);
+	}
+	intvec* newvec = vlist->array+vlist->length;
+	newvec->value = value;
+	vlist->length++;
+	pthread_mutex_unlock(&vlist->lock);
+	return;
+}
 
 //Parse Integer Sort Inputfile
 intlist * intParseInput (char *inputfile){
